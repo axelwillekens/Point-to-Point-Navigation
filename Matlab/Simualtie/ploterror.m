c0b72c19f2f05985d3da6a967eea90e0c2acc1c8
axelwillekens <axelwillekens.student@gmail.com>
2019-05-13 09:04:28 +0200

    eenheden toegevoegd aan magnfig

--- Matlab/Simualtie/ploterror.m
+++ Matlab/Simualtie/ploterror.m
@@ -2,7 +2,7 @@
 X_c=(X.Data+Y.Data)/2;
 Y_c=X_c;
 plot(sqrt((X_c-X.Data).^2+(Y_c-Y.Data).^2))
-xlabel('# metingen');
-ylabel('fout [m]');
+xlabel('# samples');
+ylabel('error [m]');
 axis([0 5000 0 0.9]);
 grid on
